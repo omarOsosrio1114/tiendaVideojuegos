a56a113b4fedc5e3f89a8b7d132bf875078bdd81
omarOsorio1114 <44420832+omarOsosrio1114@users.noreply.github.com>
2020-11-23 19:41:59 -0500

    cambio en la vista welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Tienda videojuegos
                 </div>
 
                 <div class="links">
